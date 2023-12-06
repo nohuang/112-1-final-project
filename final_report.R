20f9d703536dc5200abb64eff86774b890eeec1b
Urifoz <144878994+Urifoz@users.noreply.github.com>
2023-12-06 14:19:39 +0800

    Update final_report.R

--- final_report.R
+++ final_report.R
@@ -4,6 +4,3 @@ View(Tax)
 
 source("https://www.kltb.gov.tw/wSite/public/Attachment/010/f1701157697347.csv")
 
-# POST: get token ----
-cred_file = "tdx_credentials.json"
-access_token <- get_token(cred_file = cred_file)
