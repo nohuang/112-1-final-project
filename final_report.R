e64930815a2a7c464818c3ceb5e0c99c946fb7b3
Urifoz <144878994+Urifoz@users.noreply.github.com>
2023-12-13 14:55:29 +0800

    Update final_report.R

--- final_report.R
+++ final_report.R
@@ -1,10 +1,42 @@
 library(readr)
-Tax<- read_csv("https://raw.githubusercontent.com/Urifoz/112-1-final-project/main/111%E5%B9%B41-12%E6%9C%88%E5%A8%9B%E6%A8%82%E7%A8%85%E7%A8%85%E6%BA%90%E6%9C%88%E5%A0%B1%E8%A1%A8UTF-8.csv")
-View(Tax)
+dg6 <- readr::read_csv("https://data.cip.gov.tw/API/v1/dump/datastore/A53000000A-112010-003")
+View(dg6)
 
-Tax$...1
+dg6 |>
+  dplyr::filter(
+    類別 == "性別"
+  ) -> dg6_1
+
+dg6_1 |>
+  split(
+    dg6_1$項目別
+  ) -> split_dg6_1
+
+split_dg6_1$女$看報紙/
+  split_dg6_1$男$看報紙 -> summarise1
+
+names(summarise1) <- 
+  split_dg6_1$女$年度
+
+summarise1
 
 
-result <- grep("月合計", Tax$...1)
-print(result)
-print(Tax$營業總額[result])
+dg6 |>
+  dplyr::filter(
+    類別 == "年齡"
+  ) -> dg6_2
+
+View(dg6_2)
+
+dg6_2 |>
+  split(
+    dg6_2$項目別
+  ) -> split_dg6_2
+
+split_dg6_2$"15 ~ 19歲"$看報紙/
+  split_dg6_2$"65歲及以上"$看報紙 -> summarise2
+
+names(summarise2) <- 
+  split_dg6_2$"15 ~ 19歲"$年度
+
+summarise2
